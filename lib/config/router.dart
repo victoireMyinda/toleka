dea6f33eeed147cc436fe73c499c4a19f97b8b1b
victoireMyinda <myindavictoire@gmail.com>
2024-12-06 02:50:18 +0100

    main:update

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,15 +1,11 @@
 import 'package:get/get.dart';
 import 'package:toleka/presentation/screens/login/loginKelasi.dart';
-import 'package:toleka/splashscreen.dart';
 
 List<GetPage<dynamic>> getPages() {
   return [
+  
     GetPage(
         name: '/',
-        page: () => const SplashScreen(),
-        transition: Transition.cupertino),
-    GetPage(
-        name: '/login',
         page: () => const LoginKelasiScreen(),
         transition: Transition.cupertino),
   ];
