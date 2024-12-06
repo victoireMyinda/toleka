c6ea3cd493234cbf4aa90e2d1354eb67a1b53886
victoireMyinda <myindavictoire@gmail.com>
2024-12-06 04:32:36 +0100

    main:slider-in-home

--- lib/presentation/screens/home/homescreenkelasi.dart
+++ lib/presentation/screens/home/homescreenkelasi.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,67 +11,198 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
+  final List<String> vehicleImages = [
+    'assets/images/v1.jpg',
+    'assets/images/v2.jpg',
+    'assets/images/v3.jpg',
+  ];
+
+  late PageController _pageController;
+  int _currentPage = 0;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentPage);
+    _startAutoScroll();
+  }
+
+  void _startAutoScroll() {
+    _timer = Timer.periodic(const Duration(seconds: 3), (Timer timer) {
+      if (_currentPage < vehicleImages.length - 1) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+      _pageController.animateToPage(
+        _currentPage,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              height: 200,
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 2, 64, 34),
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(30),
-                  bottomLeft: Radius.circular(30),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Premier conteneur
+              Container(
+                height: 200,
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 2, 64, 34),
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                  ),
                 ),
-              ),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Hello, Lindsey!',
-                      style: GoogleFonts.poppins(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.grey,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Hello, Lindsey!',
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.grey,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      'Toleka, Kinshasa',
-                      style: GoogleFonts.poppins(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
+                      const SizedBox(height: 8),
+                      Text(
+                        'Toleka, Kinshasa',
+                        style: GoogleFonts.poppins(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 16),
-                    SizedBox(
-                      width: 300,
-                      height: 50, // Largeur ajustée
-                      child: TextField(
-                        decoration: InputDecoration(
-                          prefixIcon: const Icon(Icons.search),
-                          hintText: 'What service are you looking for?',
-                          hintStyle: GoogleFonts.poppins(fontSize: 14),
-                          filled: true,
-                          fillColor: Colors.grey[200],
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
-                            borderSide: BorderSide.none,
+                      const SizedBox(height: 16),
+                      SizedBox(
+                        width: 300,
+                        height: 50,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.search),
+                            hintText: 'What service are you looking for?',
+                            hintStyle: GoogleFonts.poppins(fontSize: 14),
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide.none,
+                            ),
                           ),
                         ),
                       ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // Deuxième conteneur : cartes
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: GridView.count(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  children: const [
+                    _HomeCard(
+                      text: 'Commander véhicule',
+                      icon: Icons.directions_car,
+                    ),
+                    _HomeCard(
+                      text: 'Nous contacter',
+                      icon: Icons.phone,
+                    ),
+                    _HomeCard(
+                      text: 'Catalogue',
+                      icon: Icons.book,
+                    ),
+                    _HomeCard(
+                      text: 'Partager',
+                      icon: Icons.share,
+                    ),
+                    _HomeCard(
+                      text: 'Chat avec nous',
+                      icon: Icons.chat,
+                    ),
+                    _HomeCard(
+                      text: 'Service',
+                      icon: Icons.settings,
                     ),
                   ],
                 ),
               ),
-            ),
-          ],
+
+              // Conteneur slider
+              SizedBox(
+                height: 200,
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemCount: vehicleImages.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.asset(
+                          vehicleImages[index],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+// Widget pour les cartes
+class _HomeCard extends StatelessWidget {
+  final String text;
+  final IconData icon;
+
+  const _HomeCard({required this.text, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      elevation: 4,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 36, color: Colors.green),
+          const SizedBox(height: 8),
+          Text(
+            text,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w500),
+          ),
+        ],
       ),
     );
   }
